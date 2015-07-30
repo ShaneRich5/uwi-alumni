e3c9a6a83da42d7cfcc362120dd7c76b26d412b7
Shane Richards <shane.richards121@gmail.com>
2015-07-29 21:47:46 -0500

    added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-var elixir = require('laravel-elixir');
+//var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var inject = require('gulp-inject');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +16,28 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+//elixir(function(mix) {
+//    mix.sass('app.scss');
+//});
+
+gulp.task('css', function() {
+    return gulp.src('public/css/main.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css/min'))
+        .pipe(notify({ message: 'css minified!' }));
+});
+
+gulp.task('inject', function() {
+    return gulp.src('resources/views/index.php')
+        .pipe(inject(gulp.src(['.bower_components/**/*.js'], {read: false})))
+        .pipe(gulp.dest('resources/views'));
+});
+
+gulp.task('default', function() {
+    gulp.run('css');
+    gulp.run('inject');
+
+    gulp.watch('public/css/*.css', function() {
+        gulp.run('css');
+    });
 });
